049b75d66c6cd409c1230a9fe1a67da4f4179311
Rin <shinohara-rin@users.noreply.github.com>
2024-06-29 15:20:12 +0100

    debug

--- go/adbc/driver/mocks/driver_test.go
+++ go/adbc/driver/mocks/driver_test.go
@@ -225,6 +225,9 @@ func (suite *MocksDriverTests) TestIntegers() {
 	suite.Require().NoError(err)
 	defer expectedRecords.Release()
 
+	j, _ := json.MarshalIndent(result, "", "  ")
+	fmt.Println(string(j))
+
 	suite.Truef(array.RecordEqual(expectedRecords, result), "expected: %s\ngot: %s", expectedRecords, result)
 
 	suite.False(rdr.Next())
